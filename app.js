f8f2a9a2cd9364adb17a83fed0eb3f2886db800b
Alejae1998 <alejagr1998@gmail.com>
2022-05-05 13:20:52 -0700

    add style

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const adventureHPEL = document.querySelector('#adventurer-hp');
 const adventurerImgEl = document.querySelector('#adventurer-img');
 const form = document.querySelector('form');
 const goblinListEl = document.querySelector('.goblins');
+const player = {};
 
 let defeatedGoblinsCount = 0;
-let playerHP = 10;
+player.hp = 10;
 let goblins = [
     { name: 'Cookie', hp: 1 },
     { name: 'professor goblin', hp: 4 },
@@ -44,11 +45,10 @@ function goblinClickHandler(goblin) {
 
 
     if (goblin.hp === 0) return;
-    if (playerHP === 0) return;
+    if (player.hp === 0) return;
 
     const playerHit = Math.random();
-
-    if (playerHit < 1) {
+    if (playerHit < 0.5) {
         goblin.hp--;
         displayGoblins();
         alert(`You hit ${goblin.name}!`);
@@ -62,16 +62,13 @@ function goblinClickHandler(goblin) {
     }
 
     const goblinHit = Math.random();
-    if (goblinHit < 1) {
-        playerHP--;
-
-        adventureHPEL.textContent = playerHP;
-        alert(`${goblin.name} hit you!`);
-        if (playerHP === 0) {
-            alert('Game Over 🥲');
-        }
+    if (goblinHit < 0.5) {
+        player.hp--;
+        adventureHPEL.textContent = player.hp;
+        alert(`${goblin.name} hit you! ${player.hp === 0 ? 'Game Over 🥲' : ''}`);
     } else {
         alert(`${goblin.name} tried to hit you and missed!!`);
         adventurerImgEl.classList.add('game-over');
     }
+
 }
